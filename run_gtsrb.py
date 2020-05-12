5581e53985181c6bd0cbf1a15e86c8ae6bac0b1b
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2020-05-12 18:32:38 +0900

    [update] add command line args

--- run_gtsrb.py
+++ run_gtsrb.py
@@ -2,5 +2,5 @@ from src.trains.gtsrb import train
 import sys
 
 args = sys.argv
-print('Batch Size: {0} | Epoch: {1}'.format(args[0], args[1]))
-train(batch_size=int(args[0]), epochs=int(args[1]))
+print('Batch Size: {0} | Epoch: {1}'.format(args[1], args[2]))
+train(batch_size=int(args[1]), epochs=int(args[2]))
