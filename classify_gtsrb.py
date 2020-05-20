120540b4605f4ba04167eec23a82406b9cf4e00c
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2020-05-20 13:06:09 +0900

    [update] run classification against test data and victim set

---
+++ classify_gtsrb.py
@@ -0,0 +1,27 @@
+from src.storage.datasetrepo import GTSRBRepository
+from src.utils.imgpro import *
+from src.storage.modelrepo import ModelRepository
+from src.trains.models import GTSRBCNN
+from src.utils.stats import success_rate
+import random
+import torch
+
+
+x_stop = GTSRBRepository.load_from_images(dir_name='victim-set')
+x_stop = x_stop.reshape((x_stop.shape[0], x_stop.shape[3], x_stop.shape[1], x_stop.shape[2]))
+x_stop, y_stop = resize(img=torch.tensor(scale_gtsrb(images=x_stop), dtype=torch.float32), size=(32, 32)), torch.tensor([14]*len(x_stop), dtype=torch.long)
+
+_, _, _, _, x_test, y_test = GTSRBRepository.load_from_pickle_tf()
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[3], x_test.shape[1], x_test.shape[2]))
+x_test, y_test = torch.tensor(x_test, dtype=torch.float), torch.tensor(y_test, dtype=torch.long)
+rand_index = random.sample(range(len(y_test)), 100)
+x_test, y_test = x_test[rand_index], y_test[rand_index]
+
+device = torch.device('cpu')
+model = GTSRBCNN()
+model = ModelRepository.load(filename='GTSRB/model', device=device, model=model)
+model.eval()
+
+print('Stop: size={:d} accuracy={:.3f}'.format(len(x_stop), success_rate(outputs=model(x_stop), labels=y_stop)))
+print('Test: size={:d} accuracy={:.3f}'.format(len(x_test), success_rate(outputs=model(x_test), labels=y_test)))
+print(torch.max(model(x_stop).data, 1))
