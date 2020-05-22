4f2c4044f1e106dda4c9af1dc974f8d58428925c
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2020-05-22 16:11:54 +0900

    [fix] pdg noise

--- src/attacks/pdggtsrb.py
+++ src/attacks/pdggtsrb.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import torch
-from src.attacks.tools.pgd import multi_step_attack
+from src.attacks.tools.pgd import projected_gradient_descent
 from src.attacks.gtsrb import GTSRBAttacker
 
 
@@ -13,24 +13,21 @@ class GTSRBPDGAttacker(GTSRBAttacker):
     def run(self, iteration: int, is_terminate_when_success=False):
         self.iteration = iteration
         self.is_terminated = False  # Todo: common run() method?
-        self._pick_one_data()
+        # self._pick_one_data()
         noise = torch.zeros_like(self.inputs)
-        noisy_inputs = self.inputs + noise
-        self._evaluate(outputs=self.model(noisy_inputs), loss=0.0, i=-1)
+        self._evaluate(outputs=self.model(self.inputs + noise), loss=0.0, i=-1)
 
         for i in range(iteration):
-            delta = multi_step_attack(
+            noise = projected_gradient_descent(
                 model=self.model, X=self.inputs, y=self.labels, input_range=self.input_range,
-                epsilon=8/255, alpha=2/255, num_iter=1, randomize=True
+                noise=noise, epsilon=8/255, alpha=1/255, randomize=False
             )
-            noise += delta
-            noisy_inputs += delta
 
-            self._evaluate(outputs=self.model(noisy_inputs), loss=0.00, i=i)
+            self._evaluate(outputs=self.model(self.inputs + noise), loss=0.00, i=i)
             if self.is_terminated and is_terminate_when_success:
                 break
 
-        self._save_results(noise=noise, noisy_inputs=noisy_inputs)
+        self._save_results(noise=noise, noisy_inputs=self.inputs + noise)
 
     def _pick_one_data(self):
         index = 5
@@ -40,6 +37,7 @@ class GTSRBPDGAttacker(GTSRBAttacker):
 
 
 if __name__ == '__main__':
-    for model in ['model', 'pdg_model_8', 'pdg_model_16']:
+    for model in ['model']:
         attacker = GTSRBPDGAttacker(target_class=5, model_filename=model)
-        attacker.run(iteration=40, is_terminate_when_success=True)
+        attacker.run(iteration=10, is_terminate_when_success=True)
+        exit()
--- src/attacks/tools/pgd.py
+++ src/attacks/tools/pgd.py
@@ -4,24 +4,23 @@ from torch import Tensor
 from torch.nn import Module
 
 
-def multi_step_attack(model: Module, X: Tensor, y: Tensor, input_range: tuple,
-                      epsilon=1, alpha=1, num_iter=20, randomize=False):
-    """ Construct L_inf adversarial examples on the examples X """
+def projected_gradient_descent(model: Module, X: Tensor, y: Tensor, input_range: tuple,
+                               noise: Tensor, epsilon=1/255, alpha=1/255, randomize=False):
     if torch.max(X) > input_range[1] or torch.min(X) < input_range[0]:
         raise ValueError('scaling have not been yet done.')
-    if randomize:
-        delta = torch.rand_like(X, requires_grad=True) * (input_range[1] - input_range[0]) + input_range[0]
-        delta.data = delta.data * 2 * epsilon - epsilon
-        delta.data = (delta.data + X).clamp(input_range[0], input_range[1]) - X
-    else:
-        delta = torch.zeros_like(X, requires_grad=True)
-    delta.retain_grad()
-    for t in range(num_iter):
-        inputs = X + delta
-        loss = torch.nn.CrossEntropyLoss()(model(inputs), y)
-        loss.backward(retain_graph=True)
 
-        delta.data = (delta - alpha * delta.grad.detach().sign()).clamp(-epsilon, epsilon)
-        delta.data = (delta.data + X).clamp(input_range[0], input_range[1]) - X
-        delta.grad.zero_()
+    # if randomize:
+    #     raise NotImplementedError()
+    #     # delta = torch.rand_like(X, requires_grad=True) * (input_range[1] - input_range[0]) + input_range[0]
+    #     # delta.data = delta.data * 2 * epsilon - epsilon
+    #     # delta.data = (delta.data + X).clamp(input_range[0], input_range[1]) - X
+    # else:
+    #     delta = torch.zeros_like(X, requires_grad=True)
+
+    inputs = X + noise
+    inputs.retain_grad()
+    loss = torch.nn.CrossEntropyLoss()(model(inputs), y)
+    loss.backward()
+    delta = (noise - alpha * inputs.grad.detach().sign()).clamp(-epsilon, epsilon)
+    delta = (X + delta).clamp(input_range[0], input_range[1]) - X
     return delta.detach()
